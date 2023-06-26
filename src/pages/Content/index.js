8563ede89f80dbd9152fb11553c2dbb6bd22deef
Edward Gemar <toomanyedwards@gmail.com>
2023-06-26 15:30:53 -0500

    Handles backlog mutation

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,119 @@
-import { printLine } from './modules/print';
+console.log('jce: Content script running...');
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
 
-printLine("Using the 'printLine' function from the Print Module");
+/**
+ * Gets the element that contains the backlog
+ * 
+ * @param {*} element 
+ * @returns 
+ */
+const getBacklogElement = element => {
+  return element.querySelectorAll(`*[data-test-id='software-backlog.backlog-content.scrollable']`)?.item(0);
+}
+
+/**
+ * Gets the descendent issue card elements of the backlog element
+ * 
+ * @param {*} backlogElement 
+ * @returns 
+ */
+const getIssueCardElements = backlogElement => {
+  return backlogElement.querySelectorAll(`*[data-test-id^='software-backlog.card-list.card.content-container']`);
+}
+
+/**
+ * Handles a mutation of the backlogElement
+ * 
+ * @param {*} backlogElement 
+ * @returns 
+ */
+const handleBacklogMutation = async backlogElement => {
+  const issueCardElements = getIssueCardElements(backlogElement);
+
+  getMapOfIssueCardElementsThatNeedModification(issueCardElements);
+}
+
+/**
+ * Gets a map of all issue card elements that need modification. Map is keyed by the issue ID.
+ * 
+ * @param {*} issueCardElements 
+ */
+const getMapOfIssueCardElementsThatNeedModification = issueCardElements => {
+  console.log(`jce: getMapOfIssueCardElementsThatNeedModification: processing ${issueCardElements.length} issue card elements`);
+  const issueCardElementsThatNeedModificationMap = new Map();
+
+  issueCardElements?.forEach(
+    issueCardElement => {
+      if( !isModifiedByExtension(issueCardElement)) {
+        setModifiedByExtension(issueCardElement);
+        issueCardElementsThatNeedModificationMap.set(
+          getIssueKeyFromIssueCardElememt(issueCardElement),
+          issueCardElement
+        );
+      }
+    }
+  );
+
+  console.log(`jce: getMapOfIssueCardElementsThatNeedModification: found ${issueCardElementsThatNeedModificationMap.size} issue card elements that need modification`);
+  return issueCardElementsThatNeedModificationMap;
+}
+
+/**
+ * Gets the Jira issue key from the given issue card element
+ * 
+ * @param {*} issueCardElement 
+ * @returns 
+ */
+const getIssueKeyFromIssueCardElememt = issueCardElement => {
+  
+  return issueCardElement?.getAttribute("data-test-id");
+}
+
+/**
+ * Has the element already been modified by this extension
+ * 
+ * @param {*} element 
+ * @returns 
+ */
+const isModifiedByExtension = (element) => {
+  return element?.getAttribute('modified-by-extension')
+}
+
+/**
+ * Indicate the element has already been modified by the extension
+ * 
+ * @param {
+ * } element 
+ */
+const setModifiedByExtension = (element) => {
+  element.setAttribute('modified-by-extension', 'true');
+}
+
+/**
+ * Observe mutations and update the backlog as necessary
+ */
+const observer = new MutationObserver(
+  mutations => {  
+    mutations.map(
+      mutation => {
+        const backlogNode = getBacklogElement(mutation.target);
+
+        if(backlogNode) {
+          console.log('jce: Found backlog');
+          handleBacklogMutation(backlogNode);
+        }
+      }       
+    )
+  }    
+);
+
+
+const target = document.querySelector("html");
+const config = { childList:true, subtree:true};
+
+/**
+ * Observe all mutations to the DOM
+ * TODO: Optimize this later if necessary
+ */
+
+observer.observe(target, config);
