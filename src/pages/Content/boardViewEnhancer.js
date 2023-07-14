1d0b78e778ac0c4202f9f78e7fb9d527c825823e
Edward Gemar <toomanyedwards@gmail.com>
2023-07-14 17:29:12 -0500

    Moved board view enhancements to separate file

---
+++ src/pages/Content/boardViewEnhancer.js
@@ -0,0 +1,314 @@
+import React from 'react';
+import AlertsIndicator from './AlertsIndicator';
+import { createRoot } from 'react-dom/client';
+import {JIRA_FIELD_IDS, isBug, isDone} from './jiraApiUtils'
+import { enhanceIssueCards, enhanceSelectedIssueCards, applyIssueCardEnhancements } from './jiraViewEnhancer';
+
+const BOARD_CARDS_SELECTOR = '*[data-test-id="software-board.board"] *[data-testid="platform-board-kit.ui.card.card"]';
+const ALERTS_INDICATOR_WRAPPER_ID = 'ALERTS_INDICATOR_WRAPPER_ID';
+
+/**
+ * Returns true if the Jira board is diplayed 
+ * 
+ * @returns 
+ */
+export const isBoardView = () => {
+  return !!document.querySelector('[data-test-id="software-board.board"]');
+}
+
+/**
+ * Handles mutation of the Jira board view
+ * 
+ * @param {*} mutation 
+ */
+export const handleBoardViewMutation = async (mutation) => {
+
+  enhanceSelectedIssueCards(BOARD_CARDS_SELECTOR, enhanceBoardCards);
+
+  handleBoardCardAlertIndicatorOutOfPlace(mutation);
+
+  handleBoardIssueEditorDialogClosing(mutation);
+}
+
+/**
+ * Enhances the specified board cards. New data will be retrieved.
+ * 
+ * @param {*} boardCards 
+ * @returns 
+ */
+const enhanceBoardCards = async (boardCards) => {
+  return enhanceIssueCards( 
+    boardCards,
+    getIssueKeyFromBoardCard,
+    [
+      JIRA_FIELD_IDS.ASSIGNEE,
+      JIRA_FIELD_IDS.ISSUE_TYPE,
+      JIRA_FIELD_IDS.KEY,
+      JIRA_FIELD_IDS.LABELS,
+      JIRA_FIELD_IDS.OWNER,
+      JIRA_FIELD_IDS.PAIR_ASSIGNEE, 
+      JIRA_FIELD_IDS.STATUS, 
+      JIRA_FIELD_IDS.STORY_POINT_ESTIMATE,
+      JIRA_FIELD_IDS.TESTER
+    ],
+    applyBoardCardEnhancements
+  );
+}
+
+/**
+ * Gets the Jira issue key from the given board card
+ * 
+ * @param {*} boardCard 
+ * @returns 
+ */
+const getIssueKeyFromBoardCard = boardCard => {
+  console.log(`jce: getIssueKeyFromBoardCard getAttribute ${boardCard}`);
+  const boardCardIssueKey = boardCard?.getAttribute("id").slice('card-'.length);
+  return boardCardIssueKey;
+}
+
+/**
+ * Applies enhancements to the specified card based on the data provided
+ * 
+ * @param {*} boardCard 
+ * @param {*} boardIssueData 
+ */
+const applyBoardCardEnhancements = (boardCard, boardIssueData) => {
+  applyIssueCardEnhancements(boardCard, boardIssueData, enhanceBoardCard);
+}
+
+/**
+ * Enhances a board card
+ * 
+ * @param {*} boardCard 
+ * @param {*} boardIssueData 
+ */
+const enhanceBoardCard = (boardCard, boardIssueData) => {
+
+  console.log(`jce: enhanceBoardCard 1 :${boardCard}`);
+  var cardColor = "#c1e1c1";
+
+  const alerts = getBoardIssueAlerts(boardIssueData);
+
+  console.log(`jce: enhanceBoardCard 2`);
+  if(alerts.length) {
+    cardColor = "#fafad2";
+  }
+  updateBoardCardAlertsIndicator(boardCard, alerts);
+
+  const backlogCardContainer = boardCard.querySelectorAll(`*[data-test-id='platform-card.ui.card.focus-container']`)?.item(0);
+  colorizeCard(backlogCardContainer, cardColor);
+
+}
+
+/**
+ * Get any alerts based on the specified issue data
+ * 
+ * @param {*} issueData 
+ * @returns an array of alert message strings
+ */
+const getBoardIssueAlerts = (issueData) => {
+  const boardIssueAlerts = [];
+
+  if(!issueData.fields[JIRA_FIELD_IDS.STORY_POINT_ESTIMATE] && !isBug(issueData)) {
+
+    boardIssueAlerts.push(`Needs Estimate`);
+  }
+  
+  if(!issueData.fields[JIRA_FIELD_IDS.OWNER]) {
+
+    boardIssueAlerts.push(`Needs Owner`);
+  }
+
+  if(!issueData.fields[JIRA_FIELD_IDS.TESTER]) {
+
+    boardIssueAlerts.push(`Needs Tester`);
+  }
+
+  if(!issueData.fields[JIRA_FIELD_IDS.ASSIGNEE] && !isDone(issueData)) {
+
+    boardIssueAlerts.push(`Needs Assignee`);
+  }
+  
+  return boardIssueAlerts;
+}
+
+/**
+ * Adds the alert indicator to the card if there are alerts, clears it otherwise.
+ * 
+ * @param {*} boardCard 
+ * @param {*} alerts 
+ */
+const updateBoardCardAlertsIndicator = (boardCard, alerts) => {
+  console.log(`jce: updateBoardCardAlertsIndicator 1`);
+  
+  var alertsIndicatorWrapper = getBoardCardAlertsIndicatorWrapper(boardCard);
+
+  // Remove the alerts indicator, if any
+  if(alertsIndicatorWrapper) {
+    alertsIndicatorWrapper.remove();
+  }
+  
+  // Add the alerts indicator if there are alerts
+  if(alerts.length) {
+
+    console.log(`jce: updateBoardCardAlertsIndicator 3`);
+    alertsIndicatorWrapper = document.createElement("div");
+    alertsIndicatorWrapper.setAttribute("id", ALERTS_INDICATOR_WRAPPER_ID);
+    
+    positionAlertsIndicator(boardCard, alertsIndicatorWrapper);
+
+    console.log(`jce: updateBoardCardAlertsIndicator 5`);
+    const alertsIndicatorRoot = createRoot(alertsIndicatorWrapper);
+    alertsIndicatorRoot.render(<AlertsIndicator alerts={alerts} />);
+  } 
+}
+
+/**
+ * Gets the element where the react AlertsIndicator component should be inserted
+ * 
+ * @param {*} boardCard 
+ * @returns 
+ */
+const getBoardCardAlertsIndicatorWrapper = boardCard => {
+  return boardCard?.querySelector(`[id="${ALERTS_INDICATOR_WRAPPER_ID}"]`);
+}
+
+/**
+ * Positions the alert indicator to the right of the board card assignee avatar
+ * 
+ * @param {*} boardCard 
+ * @param {*} alertsIndicatorWrapper 
+ */
+const positionAlertsIndicator = (boardCard, alertsIndicatorWrapper) => {
+  
+  var alertsIndicatorParent = null;
+  alertsIndicatorParent = boardCard.querySelector('[data-testid="software-board.common.fields.assignee-field-static.avatar-wrapper"]')?.parentElement;
+
+  if(!alertsIndicatorParent) {
+    alertsIndicatorParent = boardCard.querySelector('[data-testid="software-board.board-container.board.card-container.card.assignee-field.button"]')?.parentElement;
+  }
+
+  alertsIndicatorParent.insertAdjacentElement(`beforeend`, alertsIndicatorWrapper);
+}
+
+/**
+ * Handles when the issue editor dialog closes
+ * 
+ * @param {*} mutation 
+ */
+const handleBoardIssueEditorDialogClosing = (mutation) => {
+
+  mutation.removedNodes.forEach(
+    removedNode => {
+      console.log(`jce: NODE REMOVED`);
+      describeNode(removedNode);
+      
+      if( isBoardIssueEditorDialog(removedNode)) {
+        console.log(`jce: Board Issue Editor Closing Foo`);
+
+        const issueKey  = getIssueKeyFromBoardIssueEditorDialog(removedNode);
+        console.log(`jce: foundIssueIdContainer: ${issueKey}`);
+
+        enhanceBoardCards([getBoardCardFromIssueKey(issueKey)]);
+      }
+    }
+  );
+}
+
+/**
+ * Returns true if the node is the board issue editor dialog
+ * 
+ * @param {*} node 
+ * @returns 
+ */
+const isBoardIssueEditorDialog = (node) => {
+  return node.nodeType === Node.ELEMENT_NODE && node.getAttribute(`class`) === ` css-12aymf5`;
+}
+
+/**
+ * Gets jira issue key for the issue being edited by the editor dialog
+ * 
+ * @param {*} boardIssueEditorDialog 
+ * @returns 
+ */
+const getIssueKeyFromBoardIssueEditorDialog = (boardIssueEditorDialog) => {
+  return boardIssueEditorDialog.querySelector(`*[data-testid='issue.views.issue-base.foundation.breadcrumbs.current-issue.item'] span`).textContent;
+}
+
+/**
+ * Returns the board card for the specified issue key
+ * @param {*} issueKey 
+ * @returns 
+ */
+const getBoardCardFromIssueKey = (issueKey) => {
+  return document.querySelector(getSelectorForBoardCard(issueKey));
+}
+
+/**
+ * Repositions the alerts indicator if it was moved (by jira)
+ * 
+ * @param {*} mutation 
+ * @returns 
+ */
+const handleBoardCardAlertIndicatorOutOfPlace = (mutation) => {
+  const boardCard = getClosestBoardCard(mutation.target);
+
+  if(!boardCard) {
+    return;
+  }
+  const alertsIndicatorInsertionPoint = getBoardCardAlertsIndicatorWrapper(boardCard);
+
+  if(alertsIndicatorInsertionPoint) {
+
+    const alertsIndicatorInsertionPointParent = alertsIndicatorInsertionPoint.parentElement;
+
+    if(alertsIndicatorInsertionPointParent.lastElementChild != alertsIndicatorInsertionPoint) {
+
+      positionAlertsIndicator(boardCard, alertsIndicatorInsertionPoint);
+    }
+  }
+}
+
+
+/**
+ * Returns the board card associated with this element, if any.
+ * 
+ * @param {*} boardCardElement 
+ * @returns 
+ */
+const getClosestBoardCard = boardCardElement => {
+  return boardCardElement.closest(`[id^="card-"][data-test-id="platform-board-kit.ui.card.card"]`);
+}
+
+/**
+ * Gets the Jira board card element for the specified issue key
+ * 
+ * @param {*} issueKey 
+ * @returns 
+ */
+
+const getSelectorForBoardCard = (issueKey) => {
+  return BOARD_CARDS_SELECTOR + `[id="card-${issueKey}"]`;
+}
+
+
+/**
+ * Sets the background color of the specified issue card element
+ * 
+ * @param {*} issueCard 
+ * @param {*} color 
+ */
+const colorizeCard = (issueCard, color) => {
+  issueCard?.setAttribute("style", `background-color:${color}`);  
+}
+
+
+const describeNode = (node) => {
+  console.log(`  jce: Node: ${node.nodeName} ${node.nodeType}`);
+  if(node.attributes) {
+    for (const attr of node.attributes) {
+      console.log(`     ${attr.name}=${attr.value}`);
+    }
+  }
+}
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import AlertsIndicator from './AlertsIndicator';
-import { createRoot } from 'react-dom/client';
 import { enhanceIssueCards, enhanceSelectedIssueCards, applyIssueCardEnhancements } from './jiraViewEnhancer';
+import { isBoardView, handleBoardViewMutation } from './boardViewEnhancer';
 
 
 export const JIRA_VIEW = {
@@ -11,16 +9,10 @@ export const JIRA_VIEW = {
 };
 
 
-import {JIRA_FIELD_IDS, isBug, isDone} from './jiraApiUtils'
-
-const ENHANCED_BY_EXTENSION_ATTRIBUTE_NAME = 'ENHANCED_BY_EXTENSION';
-const ALERTS_INDICATOR_INSERTION_POINT_ID = 'ALERTS_INDICATOR_INSERTION_POINT_ID';
-
-const BOARD_CARDS_SELECTOR = '*[data-test-id="software-board.board"] *[data-testid="platform-board-kit.ui.card.card"]';
+import {JIRA_FIELD_IDS} from './jiraApiUtils'
 
 
-console.log('jce: Content script running...')
-
+console.log('jce: Content script running 4...')
 
 
 /**
@@ -41,15 +33,6 @@ const getJiraView = () => {
 }
 
 /**
- * Returns true if the Jira board is diplayed 
- * 
- * @returns 
- */
-const isBoardView = () => {
-  return !!document.querySelector('[data-test-id="software-board.board"]');
-}
-
-/**
  * Returns true if the Jira backlog is displayed
  * 
  * @returns 
@@ -58,22 +41,8 @@ const isBacklogView = () => {
   return !!document.querySelector('[data-test-id="software-backlog.backlog-content.scrollable"]');
 }
 
-/// BEGIN JIRA BOARD FUNCTIONALITY
-
-/// END JIRA BOARD FUNCTIONALITY
 
 
-
-/**
- * Gets the Jira board card element for the specified issue key
- * 
- * @param {*} issueKey 
- * @returns 
- */
-
-const getSelectorForBoardCard = (issueKey) => {
-  return BOARD_CARDS_SELECTOR + `[id="card-${issueKey}"]`;
-}
 
 /**
  * Sets the background color of the specified issue card element
@@ -85,14 +54,6 @@ const colorizeCard = (issueCard, color) => {
   issueCard?.setAttribute("style", `background-color:${color}`);  
 }
 
-/**
- * Gets the issue cards that need to be enhanced
- * 
-  * @returns 
- */
-const getUnenhancedIssueCards = (cardSelector) => {
-  return [...document.querySelectorAll(`${cardSelector}:not([${ENHANCED_BY_EXTENSION_ATTRIBUTE_NAME}])`)];
-}
 
 const enhanceBacklogCard = (backlogCard, backlogIssueData) => {
   const backlogCardContainer = backlogCard.querySelectorAll(`*[data-testid='software-backlog.card-list.card.card-contents.card-container']`)?.item(0);
@@ -104,91 +65,6 @@ const enhanceBacklogCard = (backlogCard, backlogIssueData) => {
     cardColor = "#fafad2";
   }
   colorizeCard(backlogCardContainer, cardColor);
-}
-
-const enhanceBoardCard = (boardCard, boardIssueData) => {
-
-  console.log(`jce: enhanceBoardCard 1 :${boardCard}`);
-  var cardColor = "#c1e1c1";
-
-  const alerts = getBoardIssueAlerts(boardIssueData);
-
-  console.log(`jce: enhanceBoardCard 2`);
-  if(alerts.length) {
-    cardColor = "#fafad2";
-  }
-  updateBoardCardAlertsIndicator(boardCard, alerts);
-
-  const backlogCardContainer = boardCard.querySelectorAll(`*[data-test-id='platform-card.ui.card.focus-container']`)?.item(0);
-  colorizeCard(backlogCardContainer, cardColor);
-
-}
-
-const positionAlertsIndicator = (boardCard, alertsIndicatorInsertionPoint) => {
-  
-  var alertsIndicatorParent = null;
-  alertsIndicatorParent = boardCard.querySelector('[data-testid="software-board.common.fields.assignee-field-static.avatar-wrapper"]')?.parentElement;
-  if(!alertsIndicatorParent) {
-    alertsIndicatorParent = boardCard.querySelector('[data-testid="software-board.board-container.board.card-container.card.assignee-field.button"]')?.parentElement;
-  }
-
-
-  alertsIndicatorParent.insertAdjacentElement(`beforeend`, alertsIndicatorInsertionPoint);
-}
-
-const updateBoardCardAlertsIndicator = (boardCard, alerts) => {
-  console.log(`jce: updateBoardCardAlertsIndicator 1`);
-  
-  var alertsIndicatorInsertionPoint = getBoardCardAlertsIndicatorInsertionPoint(boardCard);
-
-  if(alertsIndicatorInsertionPoint) {
-    alertsIndicatorInsertionPoint.remove();
-  }
-  
-  if(alerts.length) {
-
-    console.log(`jce: updateBoardCardAlertsIndicator 3`);
-    alertsIndicatorInsertionPoint = document.createElement("div");
-    alertsIndicatorInsertionPoint.setAttribute("id", ALERTS_INDICATOR_INSERTION_POINT_ID);
-    
-    positionAlertsIndicator(boardCard, alertsIndicatorInsertionPoint);
-
-    console.log(`jce: updateBoardCardAlertsIndicator 5`);
-    const alertsIndicatorRoot = createRoot(alertsIndicatorInsertionPoint);
-    alertsIndicatorRoot.render(<AlertsIndicator alerts={alerts} />);
-  }
-  
-}
-
-const getBoardIssueAlerts = (issueData) => {
-  const boardIssueAlerts = [];
-
-  if(!issueData.fields[JIRA_FIELD_IDS.STORY_POINT_ESTIMATE] && !isBug(issueData)) {
-
-    boardIssueAlerts.push(`Needs Estimate`);
-  }
-  
-  if(!issueData.fields[JIRA_FIELD_IDS.OWNER]) {
-
-    boardIssueAlerts.push(`Needs Owner`);
-  }
-
-  if(!issueData.fields[JIRA_FIELD_IDS.TESTER]) {
-
-    boardIssueAlerts.push(`Needs Tester`);
-  }
-
-  if(!issueData.fields[JIRA_FIELD_IDS.ASSIGNEE] && !isDone(issueData)) {
-
-    boardIssueAlerts.push(`Needs Assignee`);
-  }
-  
-
-  return boardIssueAlerts;
-}
-
-const applyBoardCardEnhancements = (boardCard, boardIssueData) => {
-  applyIssueCardEnhancements(boardCard, boardIssueData, enhanceBoardCard);
 }
 
 const applyBacklogCardEnhancements = (backlogCard, backlogIssueData) => {
@@ -215,128 +91,13 @@ const enhanceBacklogCards = async (backlogCards) => {
   );
 }
 
-const describeNode = (node) => {
-  console.log(`  jce: Node: ${node.nodeName} ${node.nodeType}`);
-  if(node.attributes) {
-    for (const attr of node.attributes) {
-      console.log(`     ${attr.name}=${attr.value}`);
-    }
-  }
-}
-
-const isBoardIssueEditorClosing = (removedNode) => {
-  return removedNode.nodeType === Node.ELEMENT_NODE && removedNode.getAttribute(`class`) === ` css-12aymf5`;
-}
-
-const getIssueKeyFromBoardIssueEditor = (boardIssueEditor) => {
-  return boardIssueEditor.querySelector(`*[data-testid='issue.views.issue-base.foundation.breadcrumbs.current-issue.item'] span`).textContent;
-}
-
-const handleBoardIssueEditorClosing = (mutation) => {
-
-  mutation.removedNodes.forEach(
-    removedNode => {
-      console.log(`jce: NODE REMOVED`);
-      describeNode(removedNode);
-      
-      if( isBoardIssueEditorClosing(removedNode)) {
-        console.log(`jce: Board Issue Editor Closing Foo`);
-
-        const issueKey  = getIssueKeyFromBoardIssueEditor(removedNode);
-        console.log(`jce: foundIssueIdContainer: ${issueKey}`);
-
-        enhanceBoardCards([getBoardCardFromIssueKey(issueKey)]);
-      }
-    }
-  );
-}
-
-const handleBoardCardAlertIndicatorOutOfPlace = (mutation) => {
-  const boardCard = getClosestBoardCard(mutation.target);
-
-  if(!boardCard) {
-    return;
-  }
-  const alertsIndicatorInsertionPoint = getBoardCardAlertsIndicatorInsertionPoint(boardCard);
-
-  if(alertsIndicatorInsertionPoint) {
-
-    const alertsIndicatorInsertionPointParent = alertsIndicatorInsertionPoint.parentElement;
-
-    if(alertsIndicatorInsertionPointParent.lastElementChild != alertsIndicatorInsertionPoint) {
-
-      positionAlertsIndicator(boardCard, alertsIndicatorInsertionPoint);
-    }
-  }
-}
-
-const getBoardCardAlertsIndicatorInsertionPoint = boardCard => {
-  return boardCard?.querySelector(`[id="ALERTS_INDICATOR_INSERTION_POINT_ID"]`);
-}
-
-const handleBoardViewMutation = async (mutation) => {
-  handleBoardIssueEditorClosing(mutation);
-  handleBoardCardAlertIndicatorOutOfPlace(mutation);
-
-  enhanceSelectedIssueCards(BOARD_CARDS_SELECTOR, enhanceBoardCards);
-}
 
 const enhanceBacklog = async () => {
   const BACKLOG_CARDS_SELECTOR = '[data-test-id="software-backlog.backlog-content.scrollable"] *[data-test-id^="software-backlog.card-list.card.content-container"]';
   return enhanceSelectedIssueCards(BACKLOG_CARDS_SELECTOR, enhanceBacklogCards);
 }
 
-const enhanceBoardCards = async (boardCards) => {
-  return enhanceIssueCards( 
-    boardCards,
-    getIssueKeyFromBoardCard,
-    [
-      JIRA_FIELD_IDS.ASSIGNEE,
-      JIRA_FIELD_IDS.ISSUE_TYPE,
-      JIRA_FIELD_IDS.KEY,
-      JIRA_FIELD_IDS.LABELS,
-      JIRA_FIELD_IDS.OWNER,
-      JIRA_FIELD_IDS.PAIR_ASSIGNEE, 
-      JIRA_FIELD_IDS.STATUS, 
-      JIRA_FIELD_IDS.STORY_POINT_ESTIMATE,
-      JIRA_FIELD_IDS.TESTER
-    ],
-    applyBoardCardEnhancements
-  );
-}
 
-
-/**
- * Returns the board card for the specified issue key
- * @param {*} issueKey 
- * @returns 
- */
-const getBoardCardFromIssueKey = (issueKey) => {
-  return document.querySelector(getSelectorForBoardCard(issueKey));
-}
-
-
-/**
- * Gets the Jira issue key from the given board card
- * 
- * @param {*} boardCard 
- * @returns 
- */
-const getIssueKeyFromBoardCard = boardCard => {
-  console.log(`jce: getIssueKeyFromBoardCard getAttribute ${boardCard}`);
-  const boardCardIssueKey = boardCard?.getAttribute("id").slice('card-'.length);
-  return boardCardIssueKey;
-}
-
-/**
- * Returns the board card associated with this element, if any.
- * 
- * @param {*} boardCardElement 
- * @returns 
- */
-const getClosestBoardCard = boardCardElement => {
-  return boardCardElement.closest(`[id^="card-"][data-test-id="platform-board-kit.ui.card.card"]`);
-}
 
 /**
  * Gets the Jira issue key from the given backlog card
