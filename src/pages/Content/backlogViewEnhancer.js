a9eded02cddf92d81b651c73b0486132202978d2
Edward Gemar <toomanyedwards@gmail.com>
2023-07-15 21:16:52 -0500

    Refactor backlog enhancements to  own source file

---
+++ src/pages/Content/backlogViewEnhancer.js
@@ -0,0 +1,167 @@
+import {JIRA_FIELD_IDS} from './jiraApiUtils'
+import { enhanceIssueCards, enhanceSelectedIssueCards, applyIssueCardEnhancements } from './jiraViewEnhancer';
+
+/**
+ * Returns true if the Jira backlog is displayed
+ * 
+ * @returns 
+ */
+export const isBacklogView = () => {
+  return !!document.querySelector('[data-test-id="software-backlog.backlog-content.scrollable"]');
+}
+
+/**
+ * Handles mutations to page that contains the Jira backlog view
+ * 
+ * @returns 
+ */
+export const handleBacklogViewMutation = async (mutation) => {
+  const BACKLOG_CARDS_SELECTOR = '[data-test-id="software-backlog.backlog-content.scrollable"] *[data-test-id^="software-backlog.card-list.card.content-container"]';
+  enhanceSelectedIssueCards(BACKLOG_CARDS_SELECTOR, enhanceBacklogCards);
+
+  handleBacklogIssuedEditor(mutation);
+}
+
+/**
+ * Enhances backlog cards
+ * 
+ * @param {*} backlogCards 
+ * @returns 
+ */
+const enhanceBacklogCards = async (backlogCards) => {
+  return enhanceIssueCards( 
+    backlogCards,
+    getIssueKeyFromBacklogCard,
+    [
+      JIRA_FIELD_IDS.ASSIGNEE,
+      JIRA_FIELD_IDS.ISSUE_TYPE,
+      JIRA_FIELD_IDS.KEY,
+      JIRA_FIELD_IDS.LABELS,
+      JIRA_FIELD_IDS.OWNER,
+      JIRA_FIELD_IDS.PAIR_ASSIGNEE, 
+      JIRA_FIELD_IDS.STATUS, 
+      JIRA_FIELD_IDS.STORY_POINT_ESTIMATE,
+      JIRA_FIELD_IDS.TESTER
+    ],
+    applyBacklogCardEnhancements
+  );
+}
+
+/**
+ * Gets the Jira issue key from the given backlog card
+ * 
+ * @param {*} backlogCard 
+ * @returns 
+ */
+const getIssueKeyFromBacklogCard = backlogCard => {
+  console.log(`jce: getIssueKeyFromBacklogCard`);
+  return backlogCard?.getAttribute("data-test-id").slice('software-backlog.card-list.card.content-container.'.length);
+}
+
+/**
+ * Applies enhancements to backlog cards
+ * 
+ * @param {*} backlogCard 
+ * @param {*} backlogIssueData 
+ */
+const applyBacklogCardEnhancements = (backlogCard, backlogIssueData) => {
+  applyIssueCardEnhancements(backlogCard, backlogIssueData, enhanceBacklogCard);
+}
+
+/**
+ * Enhances backlog cards
+ * 
+ * @param {*} backlogCard 
+ * @param {*} backlogIssueData 
+ */
+const enhanceBacklogCard = (backlogCard, backlogIssueData) => {
+  const backlogCardContainer = backlogCard.querySelectorAll(`*[data-testid='software-backlog.card-list.card.card-contents.card-container']`)?.item(0);
+
+  var cardColor;
+  if(backlogIssueData.fields[JIRA_FIELD_IDS.STORY_POINT_ESTIMATE]) {
+    cardColor = "#c1e1c1";
+  } else {
+    cardColor = "#fafad2";
+  }
+  colorizeCard(backlogCardContainer, cardColor);
+}
+
+/**
+ * Sets the background color of the specified issue card element
+ * 
+ * @param {*} issueCard 
+ * @param {*} color 
+ */
+const colorizeCard = (issueCard, color) => {
+  issueCard?.setAttribute("style", `background-color:${color}`);  
+}
+
+/**
+ * Handles issue updates from the backlog issue editor
+ * 
+ * @param {*} mutation 
+ * @returns 
+ */
+const handleBacklogIssuedEditor = (mutation) => {
+  const element = mutation.target;
+
+  const backlogIssueEditor = element.closest(`[data-testid="software-backlog.detail-view.issue-wrapper.backlog-issue"]`);
+
+  // If the backlog issue editor no-op
+  if(!backlogIssueEditor) {
+    return;
+  }
+
+  // If an <INPUT> element has been removed from the backlog issue editor, this implies that the user has
+  // finished editing an attribute of the issue. NOTE: This *doesn't* imply that the user actually made a change, 
+  // but for now we just unconditionally update the corrresponding issue card
+  const removedNodes = mutation.removedNodes;
+  if(removedNodes.length) { 
+    removedNodes.forEach(
+      node => {
+        const inputNode = node.querySelector(`input`);
+
+        // If an <INPUT> element was removed from the backlog issue editor...
+        if(inputNode) {
+          // Get the key for the issue currently being edited
+          const issueKey = backlogIssueEditor.querySelector(`[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"]`).textContent;
+
+          console.log(`jce: removed input node ${issueKey}`);
+
+          // Update the corresponding backloh issue card
+          enhanceBacklogCards([getBacklogCardFromIssueKey(issueKey)]);
+        }
+      }
+    );
+    
+  } 
+}
+
+/**
+ * Returns the backlog card for the specified issue key
+ * @param {*} issueKey 
+ * @returns 
+ */
+const getBacklogCardFromIssueKey = (issueKey) => {
+  return document.querySelector(getSelectorForBacklogCard(issueKey));
+}
+
+/**
+ * Gets the Jira backlog card element for the specified issue key
+ * 
+ * @param {*} issueKey 
+ * @returns 
+ */
+
+const getSelectorForBacklogCard = (issueKey) => {
+  return `[data-test-id="software-backlog.card-list.card.content-container.${issueKey}"]`;
+}
+
+const describeNode = (node) => {
+  console.log(`  jce: Node: ${node.nodeName} ${node.nodeType}`);
+  if(node.attributes) {
+    for (const attr of node.attributes) {
+      console.log(`     ${attr.name}=${attr.value}`);
+    }
+  }
+}
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,114 +1,8 @@
-import { enhanceIssueCards, enhanceSelectedIssueCards, applyIssueCardEnhancements } from './jiraViewEnhancer';
+import { isBacklogView, handleBacklogViewMutation } from './backlogViewEnhancer';
 import { isBoardView, handleBoardViewMutation } from './boardViewEnhancer';
 
 
-export const JIRA_VIEW = {
-  BACKLOG: "backlog",
-  BOARD:"board",
-  UNKNOWN:"unknown"
-};
-
-
-import {JIRA_FIELD_IDS} from './jiraApiUtils'
-
-
-console.log('jce: Content script running 4...')
-
-
-/**
- * Gets which Jira view is currently displayed. View currently supported are board and backlog.
- * 
- * @returns 
- */
-const getJiraView = () => {
-  if (isBacklogView()) {
-    return JIRA_VIEW.BACKLOG;
-  }
-
-  if (isBoardView()) {
-    return JIRA_VIEW.BOARD;
-  }
-
-  return JIRA_VIEW.UNKNOWN;
-}
-
-/**
- * Returns true if the Jira backlog is displayed
- * 
- * @returns 
- */
-const isBacklogView = () => {
-  return !!document.querySelector('[data-test-id="software-backlog.backlog-content.scrollable"]');
-}
-
-
-
-
-/**
- * Sets the background color of the specified issue card element
- * 
- * @param {*} issueCard 
- * @param {*} color 
- */
-const colorizeCard = (issueCard, color) => {
-  issueCard?.setAttribute("style", `background-color:${color}`);  
-}
-
-
-const enhanceBacklogCard = (backlogCard, backlogIssueData) => {
-  const backlogCardContainer = backlogCard.querySelectorAll(`*[data-testid='software-backlog.card-list.card.card-contents.card-container']`)?.item(0);
-
-  var cardColor;
-  if(backlogIssueData.fields[JIRA_FIELD_IDS.STORY_POINT_ESTIMATE]) {
-    cardColor = "#c1e1c1";
-  } else {
-    cardColor = "#fafad2";
-  }
-  colorizeCard(backlogCardContainer, cardColor);
-}
-
-const applyBacklogCardEnhancements = (backlogCard, backlogIssueData) => {
-  applyIssueCardEnhancements(backlogCard, backlogIssueData, enhanceBacklogCard);
-}
-
-
-const enhanceBacklogCards = async (backlogCards) => {
-  return enhanceIssueCards( 
-    backlogCards,
-    getIssueKeyFromBacklogCard,
-    [
-      JIRA_FIELD_IDS.ASSIGNEE,
-      JIRA_FIELD_IDS.ISSUE_TYPE,
-      JIRA_FIELD_IDS.KEY,
-      JIRA_FIELD_IDS.LABELS,
-      JIRA_FIELD_IDS.OWNER,
-      JIRA_FIELD_IDS.PAIR_ASSIGNEE, 
-      JIRA_FIELD_IDS.STATUS, 
-      JIRA_FIELD_IDS.STORY_POINT_ESTIMATE,
-      JIRA_FIELD_IDS.TESTER
-    ],
-    applyBacklogCardEnhancements
-  );
-}
-
-
-const enhanceBacklog = async () => {
-  const BACKLOG_CARDS_SELECTOR = '[data-test-id="software-backlog.backlog-content.scrollable"] *[data-test-id^="software-backlog.card-list.card.content-container"]';
-  return enhanceSelectedIssueCards(BACKLOG_CARDS_SELECTOR, enhanceBacklogCards);
-}
-
-
-
-/**
- * Gets the Jira issue key from the given backlog card
- * 
- * @param {*} backlogCard 
- * @returns 
- */
-const getIssueKeyFromBacklogCard = backlogCard => {
-  console.log(`jce: getIssueKeyFromBacklogCard`);
-  return backlogCard?.getAttribute("data-test-id").slice('software-backlog.card-list.card.content-container.'.length);
-}
+console.log('jce: Content script running 5...')
 
 
 /**
@@ -119,14 +13,13 @@ const observer = new MutationObserver(
     mutations.map(
       mutation => {
 
-        switch(getJiraView()) {
-          case JIRA_VIEW.BACKLOG:
-            enhanceBacklog(mutation);  
-            break;
-          case JIRA_VIEW.BOARD:
-            handleBoardViewMutation(mutation);  
-            break;
+        if(isBoardView()) {
+          handleBoardViewMutation(mutation);  
         }
+        else if(isBacklogView()){
+          handleBacklogViewMutation(mutation);  
+        }
+
       }       
     )
   }    
