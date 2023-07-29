47e3c143d22fb55ca74a31e63d10aac4165cf871
Edward Gemar <toomanyedwards@gmail.com>
2023-07-29 00:18:26 -0500

    Added Owner Quick FIlters

--- src/pages/Content/boardViewEnhancer.js
+++ src/pages/Content/boardViewEnhancer.js
@@ -42,7 +42,12 @@ const enhanceBoard = () => {
   quickFiltersContainer.appendChild(quickFiltersTitle);
 
   appendQuickFilterLabel(quickFiltersContainer, "OWNER:");
-  appendQuickFilterButton(quickFiltersContainer, "Test NO-OP");
+  appendQuickFilterButton(quickFiltersContainer, "Eric", "Custom filters" , "Owned By Eric");
+  appendQuickFilterButton(quickFiltersContainer, "Chris", "Custom filters" , "Owned By Chris");
+  appendQuickFilterButton(quickFiltersContainer, "Gus", "Custom filters" , "Owned By Gus");
+  appendQuickFilterButton(quickFiltersContainer, "Kei", "Custom filters" , "Owned By Kei");
+  appendQuickFilterButton(quickFiltersContainer, "Nick", "Custom filters" , "Owned By Nick");
+  appendQuickFilterButton(quickFiltersContainer, "PJ", "Custom filters" , "Owned By PJ");
   
   quickFilterWrapper.appendChild(quickFiltersContainer);
 
@@ -57,7 +62,7 @@ const appendQuickFilterLabel = (quickFiltersContainer, displayName) => {
   quickFiltersContainer.appendChild(quickFilterLabel);
 }
 
-const appendQuickFilterButton = (quickFiltersContainer, displayName) => {
+const appendQuickFilterButton = (quickFiltersContainer, displayName, filterMenuName, filterMenuItemName) => {
   const quickFilterButtonContainer = document.createElement("dd");
   quickFilterButtonContainer.setAttribute("class", `quick-filter-button-container`);  
 
@@ -65,17 +70,12 @@ const appendQuickFilterButton = (quickFiltersContainer, displayName) => {
   quickFilterButton.setAttribute("class", `quick-filter-button`);  
   quickFilterButton.textContent = displayName;
 
-  quickFilterButton.addEventListener("click", buttonPressed);
+  quickFilterButton.addEventListener("click", getFilterMenuToggler(filterMenuName, filterMenuItemName));
   quickFilterButtonContainer.appendChild(quickFilterButton);
 
   quickFiltersContainer.appendChild(quickFilterButtonContainer);
 }
 
-const buttonPressed = (e) => {
-  e.target.classList.toggle("quick-filter-button-active");
-  
-  // e.target.innerText = e.target.innerText.trim() === "OFF"?"ON":"OFF";
-}
 
 const getQuickFiltersWrapper = () => {
   return document.querySelector(`[id="${QUICK_FILTERS_WRAPPER_ID}"]`);
@@ -115,29 +115,21 @@ const quickFilterButtonStyleRules =`
   border: 1px solid transparent;
   display: inline-block;
   padding: 6px 9px;
-  /*padding: 7px 10px;*/
-  
   text-decoration: none;
 }
 .quick-filter-button:hover{
   border: 1px solid lightgray;
   border-radius: 15px 15px 15px 15px;
   display: inline-block;
-  /*padding: 7px 10px;*/
   padding: 6px 9x;
-  
   text-decoration: none;
 }
 .quick-filter-button-active{
-  color: #3b73af;
   background-color:white;
   filter: drop-shadow(2px 4px 6px blue);
-  /*border: 1px solid blue;*/
   border-radius: 15px 15px 15px 15px;
   display: inline-block;
-  /*padding: 7px 10px;*/
   padding: 6px 9px;
-  
   text-decoration: none;
 }
 `;
@@ -432,7 +424,7 @@ const handleBoardIssueEditorDialogClosing = (mutation) => {
       describeNode(removedNode);
       
       if( isBoardIssueEditorDialog(removedNode)) {
-        console.log(`jce: Board Issue Editor Closing Foo`);
+        console.log(`jce: Board Issue Editor Closing`);
 
         const issueKey  = getIssueKeyFromBoardIssueEditorDialog(removedNode);
         console.log(`jce: foundIssueIdContainer: ${issueKey}`);
@@ -444,13 +436,61 @@ const handleBoardIssueEditorDialogClosing = (mutation) => {
   );
 }
 
-const foo = async () => {
-  (await waitForElementToExist('[class="fiolfg-1 eYRRDc css-2b74hb"]')).click();
-  console.log('The element exists 1');
-  (await waitForElementToExist('[data-test-id="filters.common.ui.list.menu.list"]')).click();
-  console.log('The element exists 2');
-  (await waitForElementToExist('[data-testid="software-board.header.title.container"]')).click();
+const clickFilterMenuButton = (filterMenuName) => {
+  const filterMenuButtons = document.querySelectorAll(`[data-test-id="software-filters.ui.list-filter-container"] button`);
+
+  console.log(`jce: getFilterMenuButton: ${filterMenuButtons.length}`);
+
+  for (var i = 0; i < filterMenuButtons.length; ++i) {
+    console.log(`jce: getFilterMenuButton: ${filterMenuButtons[i].textContent}`);
+    
+    if(filterMenuButtons[i].textContent.startsWith(filterMenuName)) {
+      console.log(`jce: getFilterMenuButton: Found!`);
+      filterMenuButtons[i].click();
+      break;
+    }
+  }
 }
+
+const clickFilterMenuItem = async (filterMenuItemName) => {
+
+  console.log(`jce: getFilterMenuItemButton`);
+
+  (await waitForElementToExist(`[data-test-id="filters.common.ui.list.menu.list"]`));
+
+  const filterMenuItemEls = document.querySelectorAll(`[data-test-id="filters.common.ui.list.menu.list"]`);
+  
+  console.log(`jce: getFilterMenuItemButton: ${filterMenuItemEls.length}`);
+
+  for (var i = 0; i < filterMenuItemEls.length; ++i) {
+    console.log(`jce: getFilterMenuItemButton: ${filterMenuItemEls[i].textContent}`);
+    
+    if(filterMenuItemName === filterMenuItemEls[i].textContent) {
+      console.log(`jce: getFilterMenuItemButton: Found!`);
+      
+      const closest = filterMenuItemEls[i].closest(`[data-test-id="filters.common.ui.list.menu.list"]`);
+      console.log(`jce: getFilterMenuItemButton: Closest: ${closest}`);
+      closest.click(); 
+    }
+  }
+
+}
+
+const getFilterMenuToggler = (filterMenuName, filterMenuItemName) => {
+  return (e) => {
+    e.target.classList.toggle("quick-filter-button-active");
+    toggleFilterMenuItem(filterMenuName, filterMenuItemName);
+  }
+}
+
+const toggleFilterMenuItem = async (filterMenuName, filterMenuItemName) => {
+  clickFilterMenuButton(filterMenuName); // Open the filter menu
+
+  await clickFilterMenuItem(filterMenuItemName);
+
+  clickFilterMenuButton(filterMenuName); // Click away to close the filter menu
+}
+
 
 function waitForElementToExist(selector) {
   return new Promise(resolve => {
